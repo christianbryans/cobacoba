a590e7e4103e56c7268c4d12cb2bd350b6eb44e9
christianbryans <christianbryan263@gmail.com>
2024-11-25 14:15:23 +0700

    ubah bahasa

--- app.py
+++ app.py
@@ -99,5 +99,4 @@ def get_recipe():
         return jsonify({"status": "error", "message": f"Internal server error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
